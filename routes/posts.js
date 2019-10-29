f10ea40b537da1d6fa66b744b6d3dc57e16a77a5
abarne <celius377@gmail.com>
2019-10-29 15:45:39 -0500

    PUT edit post complete

--- routes/posts.js
+++ routes/posts.js
@@ -21,7 +21,30 @@ router.post('/', (req, res) => {
 });
 
 //Create a comment for the post with the specified id
-router.post('/:id/comments', (req, res) => {});
+router.post('/:id/comments', (req, res) => {
+	const comment = req.body;
+	const id = req.params.id;
+	postsDb
+		.findById(id)
+		.then((post) => {
+			console.log(req.body);
+			console.log(post);
+			postsDb
+				.insertComment(comment)
+				.then((comments) => {
+					res.status(201).json({
+						message: 'Comment added'
+					});
+				})
+				.catch((error) => {
+					console.log(error);
+					res.status(500).json({ message: 'Error posting comment' });
+				});
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Error retrieving the post' });
+		});
+});
 
 //return array of all the post objects contained in the database
 router.get('/', (req, res) => {
@@ -52,6 +75,15 @@ router.get('/:id', (req, res) => {
 //return array of all comment objects associated with the post with the specified id
 router.get('/:id/comments', (req, res) => {
 	const id = req.params.id;
+
+	postsDb
+		.findPostComments(id)
+		.then((comments) => {
+			res.status(200).json(comments);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Error finding comments' });
+		});
 });
 
 //removes the post with the specified id and returns the deleted post object
@@ -70,4 +102,20 @@ router.delete('/:id', (req, res) => {
 });
 
 //Updates the post with the specified id using data from the request body
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+	const id = req.params.id;
+	const postInfo = req.body;
+
+	postsDb.update(id, postInfo).then((post) => {
+		if (!post) {
+			res.status(404).json({ message: 'Post not fount' });
+		} else if (!postInfo.title || !postInfo.contents) {
+			res.status(400).json({ message: 'Please include a title and contents for the post' });
+		} else {
+			res.status(200).json({
+				message: 'The post was updated',
+				post: postInfo
+			});
+		}
+	});
+});
